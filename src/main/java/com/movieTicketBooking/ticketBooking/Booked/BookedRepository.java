71a387341e8bcf1de4e67945fce9e421428ffcc2
gpavithra673 <pavithra0607gjm@gmail.com>
2024-06-25 09:38:39 +0530

    update

--- src/main/java/com/movieTicketBooking/ticketBooking/Booked/BookedRepository.java
+++ src/main/java/com/movieTicketBooking/ticketBooking/Booked/BookedRepository.java
@@ -1,7 +1,9 @@
 package com.movieTicketBooking.ticketBooking.Booked;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookedRepository extends JpaRepository<Booked, Integer> {
 
 }
